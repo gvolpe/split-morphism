ea7a0a3e4ce40ffadc0b4175b76ee03ec5aed019
Gabriel Volpe <volpegabriel@gmail.com>
2019-02-11 13:05:57 +0900

    Adding lens package + conversion from Prism to Format

--- src/Control/Lens/Format.hs
+++ src/Control/Lens/Format.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE Rank2Types #-}
+
 module Control.Lens.Format where
+
+import Control.Lens
+import Control.Lens.Prism
+import Control.Monad.Reader (runReaderT)
+import Control.Monad.Reader.Class (MonadReader)
+import Data.Monoid (First)
 
 {- | A normalizing optic, isomorphic to Prism but with different laws, specifically `getMaybe` needs not to
 be injective; i.e., distinct inputs may have the same `getMaybe` result, which combined with a subsequent
@@ -13,4 +21,13 @@ data Format a b = Format
 -- | `getMaybe` and `reverseGet`, yielding a normalized formatted value. Subsequent getMaybe/reverseGet cycles are idempotent.
 normalize :: Format a b -> a -> Maybe a
 normalize (Format f g) x = g <$> f x
+
+-- | A Prism is trivially a Format.
+fromPrism :: Prism' a b -> Format a b
+fromPrism p = Format (f p) (g p)
+  where
+    f :: Prism' a b -> a -> Maybe b
+    f p x = x ^? p
+    g :: Prism' a b -> b -> a
+    g p = runIdentity . runReaderT (review p)
 

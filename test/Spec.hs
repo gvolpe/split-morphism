8f3db586cf584bfe88d485c2a084400a6bfdf629
Gabriel Volpe <volpegabriel@gmail.com>
2019-02-21 10:24:41 +0900

    Running all the specs

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,9 +4,13 @@ module Main (
 
 import qualified FormatSpec as F
 import qualified SplitEpiSpec as SE
+import qualified SplitMonoSpec as SM
+import qualified WedgeSpec as W
 
 main :: IO ()
 main = do
     F.checkProps
     SE.checkProps
+    SM.checkProps
+    W.checkProps
 
